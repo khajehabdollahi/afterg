725c588bd91d354caa1997fad38b26b404f53306
Hassan <pkhajeh@gmail.com>
2022-02-13 23:55:45 +0100

    new

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Yes it is going well very well");
+  res.send("Yes it is going well very well fantastic");
 });
 
 
