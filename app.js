bcd29b2bb783b8316c8df11e9f5cb34389e13cf0
Hassan <pkhajeh@gmail.com>
2022-02-13 22:18:24 +0100

    add dot env

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express')
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const session = require('express-session')
-const flash = require('connect-flash')
-
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const mailer = require("./views/mailer");
 const mailerForget = require("./views/mailerForget");
 
 const Friendship = require("./models/friendship");
- 
+
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
 
 const User = require("./models/user");
 const Text = require("./models/text");
@@ -31,26 +32,19 @@ const console = require("console");
 
 const upload = multer({ storage });
 
+const Room = require("./models/room");
+const app = express();
 
-
-
-
-
-const Room= require('./models/room')
-const app = express()
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 const sessionOptions = {
-  secret: 'secret',
+  secret: "secret",
   resave: false,
-  saveUninitialized: false
-}
+  saveUninitialized: false,
+};
 
 app.use(session(sessionOptions));
-app.use(flash())
-
-
+app.use(flash());
 
 mongoose.connect("mongodb://localhost:27017/room", {
   useNewUrlParser: true,
@@ -64,19 +58,17 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+app.get("/", (req, res) => {
+  res.send("Yes it is going well");
+});
 
+app.get("/registeraroom", async (req, res) => {
+  const room = new Room({ title: "a good room" });
+  await room.save();
 
-app.get('/', (req, res) => {
-  res.send('Yes it is going well')
-})
-
-app.get('/registeraroom',async (req, res) => {
-  const room = new Room({ title: 'a good room', })
-  await room.save()
-
-  res.send(room)
-})
+  res.send(room);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Serving on port 3000')
-})
+  console.log("Serving on port 3000");
+});
