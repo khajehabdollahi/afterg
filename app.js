3b27763337477622edbd4bb36df66d2a5db20628
Hassan <pkhajeh@gmail.com>
2022-02-18 13:42:41 +0100

    fixed DBURL

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
-const dburl = process.env.DB_URL || "mongodb://localhost:27017/backery";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/backery";
 
 
 
-mongoose.connect(dburl, {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   // useCreateIndex:true,
   useUnifiedTopology: true,
@@ -64,17 +64,17 @@ db.once("open", () => {
   console.log("Database connected yes is connected");
 });
 
-// const store = MongoDBStore.create({
-//   mongoUrl: dbUrl,
-//   touchAfter: 24 * 60 * 60,
-//   crypto: {
-//     secret: "squirrel",
-//   },
-// });
+const store = MongoDBStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: "squirrel",
+  },
+});
 
-// store.on("error", function (e) {
-//   console.log("Error to save to dataBase", e);
-// });
+store.on("error", function (e) {
+  console.log("Error to save to dataBase", e);
+});
 
 
 const secret = process.env.SECRET || "thisshouldbeabettersecret!";
