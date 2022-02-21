3e9e020374cdbe278d0f1724c01aaae21cf896a2
Hassan <pkhajeh@gmail.com>
2022-02-21 19:07:46 +0100

    jhhjj

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/school";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
-  useCreateIndex:true,
+  // useCreateIndex:true,
   useUnifiedTopology: true,
 });
 
